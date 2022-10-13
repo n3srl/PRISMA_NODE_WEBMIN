e6a65977d59a86c199f5786cc630527d4c86081d
Andrea <andrea.novati@n-3.it>
2022-10-13 15:38:49 +0200

    added front-end logic

--- js/prometheus.js
+++ js/prometheus.js
@@ -7,18 +7,19 @@ $(document).ready(function () {
 // Show Prometheus status
 function showStatus() {
     
-    $.getJSON('http://' + window.location.hostname + '/lib/prometheus/V2/prometheus/node_exporter', function(res) {
-            var node_exporter_metrics = "";
+    $.get('http://' + window.location.hostname + '/lib/prometheus/V2/prometheus/node_exporter', function(data, status) {
+            var node_exporter_metrics = data;
             
-            if(node_exporter_metrics === '') {
+            if(node_exporter_metrics === '' || status !== "success") {
                 node_exporter_metrics = "";
                 $('#status-prometheus').css({'color': '#b52c1d', 'font-weight': 'bold'}); // Stato NON ATTIVO, rosso
                 $('#status-prometheus').text("Servizio non attivo");
             } else {
                 $('#status-prometheus').css({'color': '#35b85a', 'font-weight': 'bold'}); // Stato ATTIVO, verde
                 $('#status-prometheus').text("Servizio attivo");
-            } 
-            $('#status-prometheus-description').html(node_exporter_metrics);
+                
+                $('#status-prometheus-description').html(node_exporter_metrics);
+            }
       })
       .fail(function() { 
           $('#status-prometheus').css({'color': '#b52c1d', 'font-weight': 'bold'}); // Stato NON ATTIVO, rosso
