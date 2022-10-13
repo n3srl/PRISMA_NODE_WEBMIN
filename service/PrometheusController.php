7d1c13d612130385503d61f91ea32bf936661787
Andrea <andrea.novati@n-3.it>
2022-10-13 15:16:10 +0200

    added controller function

--- service/PrometheusController.php
+++ service/PrometheusController.php
@@ -21,4 +21,19 @@ class PrometheusController extends Controller
 		echo '<script>var ObjID = ' . $par. ';</script>';
 	}
         
+      	public function node_exporterOperation() {
+		$permission = parent::securityCheck();
+		global $FreetureFinal;
+		global $params;
+		$par = 0;
+		if (isset($params[0]) && !empty($params[0])) {
+			$par = $params[0];
+		}
+                if(!$permission){
+                    @include "./view/user/login.php";
+                    exit;
+                }
+		echo '<script>var ObjID = ' . $par. ';</script>';
+	}
+        
 }
